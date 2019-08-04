6d34592b076fa96cd4cb2e6e0c0dec9ada4c8bcf
Pog8 <s3649332@student.rmit.edu.au>
2019-08-04 23:04:24 +1000

    TransposeMatrix

---
+++ LeetCode/src/Algorithm/Easy/TransposeMatrix.java
@@ -0,0 +1,15 @@
+package Algorithm.Easy;
+
+public class TransposeMatrix {
+    public int[][] transpose(int[][] A) {
+        int r = A.length;
+        int c = A[0].length;
+        int [][] B = new int[c][r];
+        for(int i=0;i<c;i++){
+            for(int j=0;j<r;j++){
+                B[i][j] = A[j][i];
+            }
+        }
+        return B;
+    }
+}
