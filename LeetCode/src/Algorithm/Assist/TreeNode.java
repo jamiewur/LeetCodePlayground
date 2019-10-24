12cecf605b86cc4efe2addfea1f74c934e689459
Dili <jamiewurmit@gmail.com>
2019-10-24 14:04:23 +1100

    Sum of Root To Leaf Binary Numbers

--- LeetCode/src/Algorithm/Assist/TreeNode.java
+++ LeetCode/src/Algorithm/Assist/TreeNode.java
@@ -4,7 +4,6 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-
     public TreeNode(int x) {
         val = x;
     }
--- LeetCode/src/Algorithm/Easy/SumRootToLeafBinaryNumbers.java
+++ LeetCode/src/Algorithm/Easy/SumRootToLeafBinaryNumbers.java
@@ -1,4 +1,21 @@
 package Algorithm.Easy;
 
+import Algorithm.Assist.TreeNode;
+
+import java.util.ArrayList;
+
 public class SumRootToLeafBinaryNumbers {
+
+    public int sumRootToLeaf(TreeNode root) {
+        return sumRootToLeaf(root, 0);
+    }
+
+    public int sumRootToLeaf(TreeNode root, int sum) {
+        if (root == null)
+            return 0;
+        sum = (sum * 2) + root.val;
+        if ((root.left == null) && (root.right == null))
+            return sum;
+        return sumRootToLeaf(root.left, sum) + sumRootToLeaf(root.right, sum);
+    }
 }
