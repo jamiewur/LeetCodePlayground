e1000e469b581eaab8ea04e3daaf9c2fcb3d2e66
Dili <jamiewurmit@gmail.com>
2020-02-18 11:22:08 +1100

    Find N Unique Integers Sum up to Zero

---
+++ LeetCode/src/Algorithm/Easy/FindNUniqueIntegersSumuptoZero.java
@@ -0,0 +1,10 @@
+package Algorithm.Easy;
+
+public class FindNUniqueIntegersSumuptoZero {
+    public int[] sumZero(int n) {
+        int[] A = new int[n];
+        for (int i = 0; i < n; ++i)
+            A[i] = i * 2 - n + 1;
+        return A;
+    }
+}
