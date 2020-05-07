6a40d66c658fc875e00dfa05a8aa9951d10ab52a
Dili <jamiewurmit@gmail.com>
2020-05-07 16:01:30 +1000

    Destination City

---
+++ LeetCode/src/Algorithm/Easy/DestinationCity.java
@@ -0,0 +1,12 @@
+package Algorithm.Easy;
+
+import java.util.*;
+
+public class DestinationCity {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set= new HashSet<>();
+        for (List<String> l: paths) set.add(l.get(1));
+        for (List<String> l: paths) set.remove(l.get(0));
+        return set.iterator().next();
+    }
+}
