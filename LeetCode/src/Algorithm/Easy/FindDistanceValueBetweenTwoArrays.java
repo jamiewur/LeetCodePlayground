e207705a9e761b4ef2c2ba00170b423b966b6ec1
Dili <jamiewurmit@gmail.com>
2020-05-15 21:14:38 +1000

    Find the Distance Value Between Two Arrays

--- LeetCode/src/Algorithm/Easy/FindDistanceValueBetweenTwoArrays.java
+++ LeetCode/src/Algorithm/Easy/FindDistanceValueBetweenTwoArrays.java
@@ -1,5 +1,16 @@
 package Algorithm.Easy;
 
 public class FindDistanceValueBetweenTwoArrays {
-    
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int res = 0;
+        for (int r1: arr1) {
+            case1: for (int r2: arr2) {
+                if ( Math.abs( r1 - r2 ) <= d) {
+                    res++;
+                    break case1;
+                }
+            }
+        }
+        return arr1.length - res;
+    }
 }
