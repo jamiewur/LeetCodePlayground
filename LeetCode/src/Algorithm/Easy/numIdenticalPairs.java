7fefb0feb1a7c8f9d528051a0e99c13fdb108194
Dili <jamiewurmit@gmail.com>
2020-07-28 17:23:21 +1000

    Number of Good Pairs

---
+++ LeetCode/src/Algorithm/Easy/numIdenticalPairs.java
@@ -0,0 +1,13 @@
+package Algorithm.Easy;
+
+public class numIdenticalPairs {
+    public int numIdenticalPairs(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length-1; ++i){
+            for (int j = i + 1; j < nums.length; j++) {
+                if ( nums[i] == nums[j] ) res++;
+            }
+        }
+        return res;
+    }
+}
