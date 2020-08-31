4f7570f59691658a6cc8492abbe1a2c586e8dcc9
Dili <jamiewurmit@gmail.com>
2020-08-31 15:07:32 +1000

    Symmetric Tree [DFS]

---
+++ LeetCode/src/Algorithm/Easy/SymmetricTree.java
@@ -0,0 +1,18 @@
+package Algorithm.Easy;
+
+import Algorithm.Assist.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetricHelper(root.left,root.right);
+    }
+
+    private boolean isSymmetricHelper(TreeNode left, TreeNode right){
+        if (left == null || right == null) return left == right;
+        if (left.val != right.val) return false;
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+    }
+}
