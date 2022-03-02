59b5add86ff358bffa3d582ca9d57871e687e2e7
Dili <dilili.work@gmail.com>
2022-03-02 16:40:21 +1100

    ConcatenationOfArray

---
+++ LeetCode/src/Algorithm/Easy/ConcatenationOfArray.java
@@ -0,0 +1,10 @@
+package Algorithm.Easy;
+
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length * 2];
+        for(int i=0;i<nums.length;i++)
+            result[i + nums.length] = result[i] = nums[i];
+        return result;
+    }
+}
