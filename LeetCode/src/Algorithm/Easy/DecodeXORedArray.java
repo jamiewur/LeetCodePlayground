1e96f7519b344914319f1d2ae2fdb9b0ba57aa33
Dili <dilili.work@gmail.com>
2022-03-07 13:37:27 +1100

    DecodeXORedArray

---
+++ LeetCode/src/Algorithm/Easy/DecodeXORedArray.java
@@ -0,0 +1,14 @@
+package Algorithm.Easy;
+
+public class DecodeXORedArray {
+    public int[] decode(int[] encoded, int first) {
+        int [] res = new int[encoded.length + 1];
+        res[0] = first;
+
+        for (int i = 1; i < encoded.length + 1; i++) {
+            res[i] = res[i-1] ^ encoded[i-1];
+        }
+
+        return res;
+    }
+}
