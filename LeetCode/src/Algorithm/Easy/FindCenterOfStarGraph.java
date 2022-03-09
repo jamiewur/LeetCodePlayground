65fa9ba4fc89fac8036c2fa1b6ffef6b120bfd0f
Dili <jamiewurmit@gmail.com>
2022-03-09 12:27:40 +1100

    Find Center of Star Graph

---
+++ LeetCode/src/Algorithm/Easy/FindCenterOfStarGraph.java
@@ -0,0 +1,9 @@
+package Algorithm.Easy;
+
+import java.util.Arrays;
+
+public class FindCenterOfStarGraph {
+    public int findCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
