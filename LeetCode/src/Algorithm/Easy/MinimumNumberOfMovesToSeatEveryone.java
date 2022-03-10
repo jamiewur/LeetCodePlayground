14f30786541300f2a2c36c330e319e3371674f84
Dili <jamiewurmit@gmail.com>
2022-03-10 14:50:18 +1100

    Minimum Number of Moves to Seat Everyone

---
+++ LeetCode/src/Algorithm/Easy/MinimumNumberOfMovesToSeatEveryone.java
@@ -0,0 +1,16 @@
+package Algorithm.Easy;
+
+import java.util.Arrays;
+
+public class MinimumNumberOfMovesToSeatEveryone {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int res = 0;
+
+        for (int i = 0; i < seats.length; ++i) {
+            res += Math.abs(seats[i] - students[i]);
+        }
+        return res;
+    }
+}
